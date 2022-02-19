cf7e025d5dd6afe665a3b8d3a9229095a865d3ac
nzepa <nzepapacome@gmail.com>
2022-02-19 02:21:44 +0100

    Add images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,6 +5,7 @@
 {{-- Cette section ira récupérer le contenu de yields('title') permettant de définir le titre de la page --}}
 @section('title', 'Hello City App')
 
+<img src="/img/cmr.png" alt="Logo du Cameroun">
 
 {{-- Cette section permet de définir le contenu de @yields('content') avec ce qui est contenu entre ses balises --}}
 @section('content')
